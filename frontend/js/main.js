014051626c9a4e8378a3a893b65bd6cc115b914a
nicolasR9322 <nicolas.a.rodriguez93@gmail.com>
2023-05-29 20:18:47 -0300

    desafio 2

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,22 +1,84 @@
+let favoritos = [];
+
 window.onload = () => {
   const app = document.getElementById("root");
   const container = document.createElement("div");
   container.setAttribute("class", "container");
   app.appendChild(container);
+  // Aqui debemos agregar nuestro fetch
 
+  fetch("http://localhost:3031/api/movies")
+    .then((response) => response.json())
+    .then((peliculas) => {
+      let data = peliculas.data;
+
+      data.forEach((movie) => {
+        const card = document.createElement("div");
+        card.setAttribute("class", "card");
+
+        const h1 = document.createElement("h1");
+        h1.textContent = movie.title;
+
+        const p = document.createElement("p");
+        p.textContent = `Rating: ${movie.rating}`;
+
+        const duracion = document.createElement("p");
+        duracion.textContent = `Duración: ${movie.length}`;
+
+        const star = document.createElement("p");
+        star.setAttribute("class", "action stars");
+        star.innerHTML = "&#10029";
+
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(p);
+
+        if (movie.genre !== null) {
+          const genero = document.createElement("p");
+          genero.textContent = `Genero: ${movie.genre.name}`;
+          card.appendChild(genero);
+        }
+        card.appendChild(duracion);
+        card.appendChild(star);
+      });
+
+      let displays = document.querySelector("#favorite");
+      if(localStorage.length === 0){
+        displays.style.display = "none"
+      } else {
+        displays.style.display = "block"
+      }
+      console.log(localStorage)
+
+      let favoriteMovie = document.querySelectorAll(".stars");
+
+      for (let i = 0; i < favoriteMovie.length; i++) {
+        const starID = favoriteMovie[i];
+
+        starID.addEventListener("click", () => {
+          favoritos.push(data[i]);
+          localStorage.setItem("movie", JSON.stringify(favoritos)); 
+          console.log(JSON.parse(localStorage.getItem("movie")));
+          
+        });
+      }
+    });
+};
+
+
+/* let favoritos = [];
+
+window.onload = () => {
+  const app = document.getElementById("root");
+  const container = document.createElement("div");
+  container.setAttribute("class", "container");
+  app.appendChild(container);
   // Aqui debemos agregar nuestro fetch
 
 fetch("http://localhost:3031/api/movies")
   .then((response) => response.json())
-  .then((data) => {
-    let movies = data.data
-    console.log(movies)
-    movies.forEach(movie => {
-      container.innerHTML += ` <li> ${movie.title} </li> `
-    });
-  })
-
-  /** Codigo que debemos usar para mostrar los datos en el frontend
+  .then((peliculas) => {
+    
     let data = peliculas.data;
 
     data.forEach((movie) => {
@@ -31,16 +93,56 @@ fetch("http://localhost:3031/api/movies")
 
       const duracion = document.createElement("p");
       duracion.textContent = `Duración: ${movie.length}`;
+      
+      const star = document.createElement("p");
+      star.setAttribute("class", "action stars");
+      star.innerHTML = "&#10029";
 
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
+      
       if (movie.genre !== null) {
         const genero = document.createElement("p");
         genero.textContent = `Genero: ${movie.genre.name}`;
         card.appendChild(genero);
       }
       card.appendChild(duracion);
+      card.appendChild(star)
+
+      
     });
-  */
+    
+    let favoriteMovie = document.querySelectorAll(".stars");
+
+    for (let i = 0; i < favoriteMovie.length; i++) {
+      const starID = favoriteMovie[i];
+      
+      starID.addEventListener("click", () => {
+
+        // favoritos.push(data[i])
+        favoritos.push(data[i]);
+        console.log(favoritos);
+      })
+      
+    }
+    return favoritos;
+    
+    window.addEventListener("keypress", () => {
+      
+      console.log(localStorage.getItem("movie"));
+      console.log(localStorage)
+    })
+    
+    
+  })
 };
+
+
+window.addEventListener("keypress", () => {
+  
+  console.log(localStorage.movie);
+  console.log(localStorage)
+  console.log(favoritos)
+})
+localStorage.setItem("movie",favoritos); */
--- readme.md
+++ readme.md
@@ -16,4 +16,12 @@ asíncrona desde el front-end usando fetch API
                 -modificar  una pelicula a traves de la ruta : router.put('/update/:id', moviesAPIController.update);
             -utilizar el formulario para poder crear una pelicula por medio del endpoint router.post('/create', moviesAPIController.create);
             -utilizar el formulario para borrar una pelicula a traves de un endpoint
+    
+    desafio 2:
+        -Tendremos que capturar el evento click sobre cada estrellita (ojo que puede haber
+            muchas estrellitas)
+        - Listar las películas que han sido marcadas como favoritas por el usuario.
+        - Si aún no tuviese ninguna película, esta página debe indicar que aún no tiene nada.
+        - Agregar en el encabezado de home.html un botón a “Mis películas favoritas”, Este botón solo debe aparecer si el usuario ya tiene películas favoritas.
+
             
